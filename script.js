299739e29c7e34183be70d9410e7d2c5af60fdd3
Fernando Morejon <Fernando.Morejon@itksquare.edu.mx>
2022-11-24 15:36:21 -0600

    navbar pops2

--- script.js
+++ script.js
@@ -2,7 +2,18 @@
 function musicPlay() {
   const audio = document.querySelector(".audio");
   audio.play();
-  audio.volume = 0.01;
+  audio.volume = 0.05;
 }
 
 document.addEventListener("click", musicPlay);
+
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.querySelector('.navbar').style.top = "0";
+  } else {
+    document.querySelector('.navbar').style.top = "-50px";
+  }
+}
