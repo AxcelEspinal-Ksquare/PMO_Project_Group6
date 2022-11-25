a4d5eddebacbc514198cbdcefee0d31311eca94d
Fernando Morejon <Fernando.Morejon@itksquare.edu.mx>
2022-11-25 15:31:40 -0600

    navbaraddedlogo

--- script.js
+++ script.js
@@ -14,6 +14,6 @@ function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
     document.querySelector('.navbar').style.top = "0";
   } else {
-    document.querySelector('.navbar').style.top = "-50px";
+    document.querySelector('.navbar').style.top = "-150px";
   }
 }
